912befbf1afae96e887ecf8a935b987676700a86
Dmitry Kalinin <kalinin.mitko@gmail.com>
2025-04-08 00:45:42 +0300

    Initial project

--- examples/script.py
+++ examples/script.py
@@ -1,9 +1,3 @@
-# tactics-drawer
-
-Tactics Drawer 
-
-
-```python
 import cv2
 
 from tactics_drawer.utils import TacticsDrawer
@@ -15,6 +9,3 @@ pitch.draw_filled_circle((50, 50), radius=12, color="red")
 pitch.draw_cross((150, 150), size=10, color="green")
 
 pitch.save("annotated_output.jpg")
-```
-
-![area](examples/annotated_output.jpg?raw=true)
